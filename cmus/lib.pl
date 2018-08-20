3187d6b0bd41bd271b6112909eda0d272ab22a92
Guilberc <guilber.correia@hotmail.com>
2018-08-20 20:13:39 -0300

    V1

---
+++ cmus/lib.pl
@@ -0,0 +1,130 @@
+/home/guilberc/Música/terererere.mp3
+/home/guilberc/Música/Cage The Elephant - Unpeeled/1 - Cage The Elephant - Cry Baby (Unpeeled).mp3
+/home/guilberc/Música/Cage The Elephant - Unpeeled/2 - Cage The Elephant - Whole Wide World (Unpeeled).mp3
+/home/guilberc/Música/Cage The Elephant - Unpeeled/3 - Cage The Elephant - Sweetie Little Jean (Unpeeled).mp3
+/home/guilberc/Música/Cage The Elephant - Unpeeled/4 - Cage The Elephant - Spiderhead (Unpeeled).mp3
+/home/guilberc/Música/Cage The Elephant - Unpeeled/5 - Cage The Elephant - Take It or Leave It (Unpeeled).mp3
+/home/guilberc/Música/Cage The Elephant - Unpeeled/6 - Cage The Elephant - Too Late To Say Goodbye (Unpeeled).mp3
+/home/guilberc/Música/Cage The Elephant - Unpeeled/7 - Cage The Elephant - Punchin' Bag (Unpeeled).mp3
+/home/guilberc/Música/Cage The Elephant - Unpeeled/8 - Cage The Elephant - Shake Me Down (Unpeeled).mp3
+/home/guilberc/Música/Cage The Elephant - Unpeeled/9 - Cage The Elephant - Telescope (Unpeeled).mp3
+/home/guilberc/Música/Cage The Elephant - Unpeeled/10 - Cage The Elephant - Instant Crush (Unpeeled).mp3
+/home/guilberc/Música/Cage The Elephant - Unpeeled/11 - Cage The Elephant - Trouble (Unpeeled).mp3
+/home/guilberc/Música/Cage The Elephant - Unpeeled/12 - Cage The Elephant - Ain't No Rest for the Wicked (Unpeeled).mp3
+/home/guilberc/Música/Deftones - Gore/1 - Deftones - Prayers _ Triangles.mp3
+/home/guilberc/Música/Deftones - Gore/2 - Deftones - Acid Hologram.mp3
+/home/guilberc/Música/Deftones - Gore/3 - Deftones - Doomed User.mp3
+/home/guilberc/Música/Deftones - Gore/4 - Deftones - Geometric Headdress.mp3
+/home/guilberc/Música/Deftones - Gore/5 - Deftones - Hearts _ Wires.mp3
+/home/guilberc/Música/Deftones - Gore/6 - Deftones - Pittura Infamante.mp3
+/home/guilberc/Música/Deftones - Gore/7 - Deftones - Xenon.mp3
+/home/guilberc/Música/Deftones - Gore/8 - Deftones - (L)MIRL.mp3
+/home/guilberc/Música/Deftones - Gore/9 - Deftones - Gore.mp3
+/home/guilberc/Música/Deftones - Gore/10 - Deftones - Phantom Bride.mp3
+/home/guilberc/Música/Deftones - Gore/11 - Deftones - Rubicon.mp3
+/home/guilberc/Música/Eldamar - A Dark Forgotten Past/1 - Return of Darkness.mp3
+/home/guilberc/Música/Eldamar - A Dark Forgotten Past/2 - Another Journey Begins.mp3
+/home/guilberc/Música/Eldamar - A Dark Forgotten Past/3 - In Search for New Wisdom.mp3
+/home/guilberc/Música/Eldamar - A Dark Forgotten Past/4 - A Secret by the Branches.mp3
+/home/guilberc/Música/Eldamar - A Dark Forgotten Past/5 - Ancient Sorcery.mp3
+/home/guilberc/Música/Eldamar - A Dark Forgotten Past/6 - The Passing.mp3
+/home/guilberc/Música/Eldamar - A Dark Forgotten Past/7 - New Understanding.mp3
+/home/guilberc/Música/Filter - The Trouble With Angels/1 - The Inevitable Relapse.mp3
+/home/guilberc/Música/Filter - The Trouble With Angels/2 - Drug Boy.mp3
+/home/guilberc/Música/Filter - The Trouble With Angels/3 - Absentee Father.mp3
+/home/guilberc/Música/Filter - The Trouble With Angels/4 - No Love.mp3
+/home/guilberc/Música/Filter - The Trouble With Angels/5 - No Re-Entry.mp3
+/home/guilberc/Música/Filter - The Trouble With Angels/6 - Down With Me.mp3
+/home/guilberc/Música/Filter - The Trouble With Angels/7 - Catch a Falling Knife.mp3
+/home/guilberc/Música/Filter - The Trouble With Angels/8 - The Trouble With Angels.mp3
+/home/guilberc/Música/Filter - The Trouble With Angels/9 - Clouds.mp3
+/home/guilberc/Música/Filter - The Trouble With Angels/10 - Fades Like a Photograph (Dead Angel).mp3
+/home/guilberc/Música/Limp Bizkit - Rock Im Park/1 - Hot Dog.mp3
+/home/guilberc/Música/Limp Bizkit - Rock Im Park/2 - Show Me What You Got.mp3
+/home/guilberc/Música/Limp Bizkit - Rock Im Park/3 - Break Stuff.mp3
+/home/guilberc/Música/Limp Bizkit - Rock Im Park/4 - The One.mp3
+/home/guilberc/Música/Limp Bizkit - Rock Im Park/5 - Livin' It Up.mp3
+/home/guilberc/Música/Limp Bizkit - Rock Im Park/6 - My Generation.mp3
+/home/guilberc/Música/Limp Bizkit - Rock Im Park/7 - Re-Arranged.mp3
+/home/guilberc/Música/Limp Bizkit - Rock Im Park/8 - Master of Puppets.mp3
+/home/guilberc/Música/Limp Bizkit - Rock Im Park/9 - Faith.mp3
+/home/guilberc/Música/Limp Bizkit - Rock Im Park/10 - Full Nelson.mp3
+/home/guilberc/Música/Limp Bizkit - Rock Im Park/11 - My Way.mp3
+/home/guilberc/Música/Limp Bizkit - Rock Im Park/12 - Nookie.mp3
+/home/guilberc/Música/Limp Bizkit - Rock Im Park/13 - I Would for You.mp3
+/home/guilberc/Música/Limp Bizkit - Rock Im Park/14 - Take a Look Around.mp3
+/home/guilberc/Música/Limp Bizkit - Rock Im Park/15 - Rollin'.mp3
+/home/guilberc/Música/Metallica - Death Magnetic/1 - That Was Just Your Life.mp3
+/home/guilberc/Música/Metallica - Death Magnetic/2 - The End Of The Line.mp3
+/home/guilberc/Música/Metallica - Death Magnetic/3 - Broken, Beat & Scarred.mp3
+/home/guilberc/Música/Metallica - Death Magnetic/4 - The Day That Never Comes.mp3
+/home/guilberc/Música/Metallica - Death Magnetic/5 - All Nightmare Long.mp3
+/home/guilberc/Música/Metallica - Death Magnetic/6 - Cyanide.mp3
+/home/guilberc/Música/Metallica - Death Magnetic/7 - The Unforgiven III.mp3
+/home/guilberc/Música/Metallica - Death Magnetic/8 - The Judas Kiss.mp3
+/home/guilberc/Música/Metallica - Death Magnetic/9 - Suicide & Redemption.mp3
+/home/guilberc/Música/Metallica - Death Magnetic/10 - My Apocalypse.mp3
+/home/guilberc/Música/Nargaroth - Era of Threnody/1 - Dawn of Epiphany.mp3
+/home/guilberc/Música/Nargaroth - Era of Threnody/2 - Whither Goest Thou.mp3
+/home/guilberc/Música/Nargaroth - Era of Threnody/3 - Conjunction Underneath the Alpha Weel.mp3
+/home/guilberc/Música/Nargaroth - Era of Threnody/4 - ...as Orphans Drifting in a Desert Night.mp3
+/home/guilberc/Música/Nargaroth - Era of Threnody/5 - The Agony of a Dying Phoenix.mp3
+/home/guilberc/Música/Nargaroth - Era of Threnody/6 - Epicedium to a Broken Dream.mp3
+/home/guilberc/Música/Nargaroth - Era of Threnody/7 - Love Is a Dog from Hell.mp3
+/home/guilberc/Música/Seether - Poison The Parish/1 - Stoke The Fire.mp3
+/home/guilberc/Música/Seether - Poison The Parish/2 - Betray And Degrade.mp3
+/home/guilberc/Música/Seether - Poison The Parish/3 - Something Else.mp3
+/home/guilberc/Música/Seether - Poison The Parish/4 - I'll Survive.mp3
+/home/guilberc/Música/Seether - Poison The Parish/5 - Let You Down.mp3
+/home/guilberc/Música/Seether - Poison The Parish/6 - Against The Wall.mp3
+/home/guilberc/Música/Seether - Poison The Parish/7 - Let Me Heal.mp3
+/home/guilberc/Música/Seether - Poison The Parish/8 - Saviours.mp3
+/home/guilberc/Música/Seether - Poison The Parish/9 - Nothing Left.mp3
+/home/guilberc/Música/Seether - Poison The Parish/10 - Count Me Out.mp3
+/home/guilberc/Música/Seether - Poison The Parish/11 - Emotionless.mp3
+/home/guilberc/Música/Seether - Poison The Parish/12 - Sell My Soul.mp3
+/home/guilberc/Música/Silverchair - Frogstomp 20th Anniversary (Deluxe Edition Remastered)/1 - Israel's Son (Remastered).mp3
+/home/guilberc/Música/Silverchair - Frogstomp 20th Anniversary (Deluxe Edition Remastered)/2 - Tomorrow (Remastered).mp3
+/home/guilberc/Música/Silverchair - Frogstomp 20th Anniversary (Deluxe Edition Remastered)/3 - Faultline (Remastered).mp3
+/home/guilberc/Música/Silverchair - Frogstomp 20th Anniversary (Deluxe Edition Remastered)/4 - Pure Massacre (Remastered).mp3
+/home/guilberc/Música/Silverchair - Frogstomp 20th Anniversary (Deluxe Edition Remastered)/5 - Shade (Remastered).mp3
+/home/guilberc/Música/Silverchair - Frogstomp 20th Anniversary (Deluxe Edition Remastered)/6 - Leave Me Out (Remastered).mp3
+/home/guilberc/Música/Silverchair - Frogstomp 20th Anniversary (Deluxe Edition Remastered)/7 - Suicidal Dream (Remastered).mp3
+/home/guilberc/Música/Silverchair - Frogstomp 20th Anniversary (Deluxe Edition Remastered)/8 - Madman (Remastered).mp3
+/home/guilberc/Música/Silverchair - Frogstomp 20th Anniversary (Deluxe Edition Remastered)/9 - Undecided (Remastered).mp3
+/home/guilberc/Música/Silverchair - Frogstomp 20th Anniversary (Deluxe Edition Remastered)/10 - Cicada (Remastered).mp3
+/home/guilberc/Música/Silverchair - Frogstomp 20th Anniversary (Deluxe Edition Remastered)/11 - Findaway (Remastered).mp3
+/home/guilberc/Música/Silverchair - Frogstomp 20th Anniversary (Deluxe Edition Remastered)/1 - Tomorrow (Single Version) [Remastered].mp3
+/home/guilberc/Música/Silverchair - Frogstomp 20th Anniversary (Deluxe Edition Remastered)/2 - Acid Rain (Remastered).mp3
+/home/guilberc/Música/Silverchair - Frogstomp 20th Anniversary (Deluxe Edition Remastered)/3 - Blind (Remastered).mp3
+/home/guilberc/Música/Silverchair - Frogstomp 20th Anniversary (Deluxe Edition Remastered)/4 - Stoned (Remastered).mp3
+/home/guilberc/Música/Silverchair - Frogstomp 20th Anniversary (Deluxe Edition Remastered)/5 - Madman (Vocal Version Remastered).mp3
+/home/guilberc/Música/Silverchair - Frogstomp 20th Anniversary (Deluxe Edition Remastered)/6 - Madman (Live at the Cambridge Remastered).mp3
+/home/guilberc/Música/Silverchair - Frogstomp 20th Anniversary (Deluxe Edition Remastered)/7 - Blind (Live at the Cambridge Remastered).mp3
+/home/guilberc/Música/Tame Impala - Currents/1 - Tame Impala - Let It Happen.mp3
+/home/guilberc/Música/Tame Impala - Currents/2 - Tame Impala - Nangs.mp3
+/home/guilberc/Música/Tame Impala - Currents/3 - Tame Impala - The Moment.mp3
+/home/guilberc/Música/Tame Impala - Currents/4 - Tame Impala - Yes I'm Changing.mp3
+/home/guilberc/Música/Tame Impala - Currents/5 - Tame Impala - Eventually.mp3
+/home/guilberc/Música/Tame Impala - Currents/6 - Tame Impala - Gossip.mp3
+/home/guilberc/Música/Tame Impala - Currents/7 - Tame Impala - The Less I Know The Better.mp3
+/home/guilberc/Música/Tame Impala - Currents/8 - Tame Impala - Past Life.mp3
+/home/guilberc/Música/Tame Impala - Currents/9 - Tame Impala - Disciples.mp3
+/home/guilberc/Música/Tame Impala - Currents/10 - Tame Impala - 'Cause I'm A Man.mp3
+/home/guilberc/Música/Tame Impala - Currents/11 - Tame Impala - Reality In Motion.mp3
+/home/guilberc/Música/Tame Impala - Currents/12 - Tame Impala - Love_Paranoia.mp3
+/home/guilberc/Música/Tame Impala - Currents/13 - Tame Impala - New Person, Same Old Mistakes.mp3
+/home/guilberc/Música/The Raconteurs - Consolers Of The Lonely/1 - The Raconteurs - Consoler Of The Lonely.mp3
+/home/guilberc/Música/The Raconteurs - Consolers Of The Lonely/2 - The Raconteurs - Salute Your Solution.mp3
+/home/guilberc/Música/The Raconteurs - Consolers Of The Lonely/3 - The Raconteurs - You Don't Understand Me.mp3
+/home/guilberc/Música/The Raconteurs - Consolers Of The Lonely/4 - The Raconteurs - Old Enough.mp3
+/home/guilberc/Música/The Raconteurs - Consolers Of The Lonely/5 - The Raconteurs - The Switch And The Spur.mp3
+/home/guilberc/Música/The Raconteurs - Consolers Of The Lonely/6 - The Raconteurs - Hold Up.mp3
+/home/guilberc/Música/The Raconteurs - Consolers Of The Lonely/7 - The Raconteurs - Top Yourself.mp3
+/home/guilberc/Música/The Raconteurs - Consolers Of The Lonely/8 - The Raconteurs - Many Shades Of Black.mp3
+/home/guilberc/Música/The Raconteurs - Consolers Of The Lonely/9 - The Raconteurs - Five On The Five.mp3
+/home/guilberc/Música/The Raconteurs - Consolers Of The Lonely/10 - The Raconteurs - Attention.mp3
+/home/guilberc/Música/The Raconteurs - Consolers Of The Lonely/11 - The Raconteurs - Pull This Blanket Off.mp3
+/home/guilberc/Música/The Raconteurs - Consolers Of The Lonely/12 - The Raconteurs - Rich Kid Blues.mp3
+/home/guilberc/Música/The Raconteurs - Consolers Of The Lonely/13 - The Raconteurs - These Stones Will Shout.mp3
+/home/guilberc/Música/The Raconteurs - Consolers Of The Lonely/14 - The Raconteurs - Carolina Drama.mp3
---
+++ cmus/playlist.pl
@@ -0,0 +1,2 @@
+/home/guilberc/Música/Tame Impala - Currents/2 - Tame Impala - Nangs.mp3
+/home/guilberc/Música/Tame Impala - Currents/1 - Tame Impala - Let It Happen.mp3
